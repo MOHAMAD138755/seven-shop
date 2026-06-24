51debb3388c9955435757dcb89eecfec766c3bbc
mohamadtorki <mohamadprograming123@gmail.com>
2026-06-24 19:37:16 +0330

    update footer section

--- resources/views/main/partials/footer.blade.php
+++ resources/views/main/partials/footer.blade.php
@@ -1,6 +1,6 @@
 <footer id="footer" class="footer">
     <div class="footer-grid">
-
+{{--@dd($settings)--}}
         <div>
             <h3>سون شاپ</h3>
             <p>بهترین تجربه خرید آنلاین</p>
@@ -8,15 +8,15 @@
 
         <div>
             <h4>لینک‌ها</h4>
-            <a href="#">خانه</a>
-            <a href="#">محصولات</a>
-            <a href="#">تماس</a>
+            <p>{{ $settings['instagram'] }}</p>
+            <p>{{ $settings['telegram'] }}</p>
+            <p>{{ $settings['youtube'] }}</p>
         </div>
 
         <div>
             <h4>پشتیبانی</h4>
-            <a href="#">سوالات متداول</a>
-            <a href="#">تماس با ما</a>
+            <p>{{ $settings['phone'] }}</p>
+            <p>{{ $settings['email'] }}</p>
         </div>
 
     </div>
